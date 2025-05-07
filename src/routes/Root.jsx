6cb48f041edbc40b4e7ccbeb7ea17f2456b9076e
bendee <benbanj@hotmail.com>
2025-05-07 23:30:41 +0100

    Adding css modules

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,9 +1,10 @@
 import { Outlet, Link } from "react-router-dom"
+import styles from './Root.module.css'
 
 function Root() {
   return (
     <>
-      <nav>
+      <nav className={styles.navbar}>
         <ul>
           <li><Link to={'/'}>Home</Link></li>
           <li><Link to={'shop'}>Shop</Link></li>
