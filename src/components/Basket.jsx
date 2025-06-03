29673ef9c6f0e77a8db8d8ff396f7711827095dc
bendee <benbanj@hotmail.com>
2025-06-03 23:13:20 +0100

    Adding rudimentary styling to the basket page
    - Adding some base styling modules for the basket and basket card
    - Adding a total to the basket

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -1,12 +1,22 @@
 import { useOutletContext } from "react-router-dom";
 import BasketCard from "./BasketCard";
+import styles from "./Basket.module.css"
 
 function Basket() {
   const { basket } = useOutletContext()
-  console.log(basket)
+  const basketTotal = calcTotal()
+
+  function calcTotal() {
+    console.log(basket)
+    return basket.reduce((sum, prod) => {
+      return sum += prod.quantity * prod.price;
+    }, 0)
+  }
+
   return (
-    <div>
+    <div className={styles.basket}>
       { basket.map(product => <BasketCard key={product.id} product={product} />) }
+      <div className={styles.basket_total}>Total: £{basketTotal}</div>
     </div>
   )
 }
--- src/components/BasketCard.jsx
+++ src/components/BasketCard.jsx
@@ -1,8 +1,10 @@
+import styles from "./BasketCard.module.css"
+
 function BasketCard({product}) {
   return (
-    <div>
-      <img className={''} src={product.image} alt={product.title} />
-      <h2 className={''}>{product.title}</h2>
+    <div className={styles.basket_card}>
+      <img className={styles.product_image} src={product.image} alt={product.title} />
+      <h2 className={styles.product_title}>{product.title}</h2>
       <p>{product.quantity} item{product.quantity > 1 ? 's' : ''}</p>
       <p>Total: £{product.quantity * product.price}</p>
     </div>
